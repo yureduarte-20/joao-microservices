710e882261908fcdfa332947a7209ead8c69549f
Yure Samarone <yuresamarone34@gmail.com>
2023-02-25 13:00:10 -0300

    feature: acesso aos problemas para os orientadores

---
+++ problems-service/src/controllers/advisor-problems.controller.ts
@@ -0,0 +1,74 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getModelSchemaRef,
+  patch,
+  put,
+  del,
+  requestBody,
+  response,
+} from '@loopback/rest';
+import { Problem } from '../models';
+import { ProblemRepository } from '../repositories';
+
+export class AdvisorProblemsController {
+  constructor(
+    @repository(ProblemRepository)
+    public problemRepository: ProblemRepository,
+  ) { }
+
+  @get('/advisor/problems/count')
+  @response(200, {
+    description: 'Problem model count',
+    content: { 'application/json': { schema: CountSchema } },
+  })
+  async count(
+    @param.where(Problem) where?: Where<Problem>,
+  ): Promise<Count> {
+    return this.problemRepository.count(where);
+  }
+
+  @get('/advisor/problems')
+  @response(200, {
+    description: 'Array of Problem model instances',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(Problem, { includeRelations: true }),
+        },
+      },
+    },
+  })
+  async find(
+    @param.filter(Problem) filter?: Filter<Problem>,
+  ): Promise<any[]> {
+    ;
+    return (await this.problemRepository.find(filter)).map(item => Object.assign({}, item))
+  }
+
+  @get('/advisor/problems/{id}')
+  @response(200, {
+    description: 'Problem model instance',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(Problem, { includeRelations: true }),
+      },
+    },
+  })
+  async findById(
+    @param.path.string('id') id: string,
+    @param.filter(Problem, { exclude: 'where' }) filter?: FilterExcludingWhere<Problem>
+  ): Promise<Problem> {
+    return Object.assign({}, await this.problemRepository.findById(id, filter));
+  }
+}
--- problems-service/src/controllers/index.ts
+++ problems-service/src/controllers/index.ts
@@ -2,3 +2,4 @@ export * from './ping.controller';
 export * from './problems-admin.controller';export * from './problems-controller.controller';
 export * from './problem-submission.controller';
 
+export * from './advisor-problems.controller';
