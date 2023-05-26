2c7ac38b4eab8f411cce5f7788e78912badeb5d3
yure <yuresamarone34@gmail.com>
2023-05-26 18:20:16 -0300

    feature: nova api gateway

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -1,15 +1,46 @@
 import Express, { Request, Response, NextFunction, } from "express";
-import router from "./routes";
 import cors from 'cors'
-
+import httpProxy from 'express-http-proxy'
+import proxyReqOptDecorator from "./handlers/HeadersHandler";
+import ServersErrors from "./handlers/ServersErrors";
 const PORT = parseInt(process.env.API_GATEWAY_PORT as string) || 3000
 const HOST = '0.0.0.0'
 const app = Express();
 
+function selectProxyHost(req: Request) {
+    let path = req.path
+    if (path.startsWith('/admin') || path.startsWith('/advisor')) {
+        path = '/' + path.split('/')[2]
+        console.log(path)
+    }
+
+    if (path.startsWith('/users'))
+        return process.env.USER_SERVICE_URL as string;
+    else if (path.startsWith('/problems') || path.startsWith('/submissions'))
+        return process.env.PROBLEMS_SERVICE_URL as string;
+    else if (path.startsWith('/doubts') || path.startsWith('/doubt'))
+        return process.env.CHAT_SERVICE_URL as string;
+    else if (path.startsWith('/login'))
+        return process.env.USER_SERVICE_URL as string;
+    else if (path.startsWith('/signup'))
+        return process.env.USER_SERVICE_URL as string;
+    else if (path.startsWith('/profile'))
+        return process.env.USER_SERVICE_URL as string;
+    else return process.env.USER_SERVICE_URL as string
+}
+
 app.use(Express.json())
 app.use(cors({ origin: process.env.FRONT_CORS_ORIGIN ?? 'http://localhost:3000' }))
-app.use(router)
-console.log(process.env.USER_SERVICE_API_URL)
-app.listen(PORT, HOST,() =>{
+app.use((req, res, next) => {
+    return httpProxy(selectProxyHost(req), {
+        proxyReqOptDecorator: proxyReqOptDecorator,
+        userResDecorator(proxyRes, proxyResData, userReq, userRes) {
+            userRes.setHeader('Content-Type', 'application/json')
+            return proxyResData
+        },
+        proxyErrorHandler: ServersErrors
+    })(req, res, next);
+});
+app.listen(PORT, HOST, () => {
     console.log(``)
 })
--- api-gateway/src/middlewares/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './AuthMiddleware'
-export * from './AuthorizationMiddleware'
-export * from './ResourceOwnerMiddleware'
